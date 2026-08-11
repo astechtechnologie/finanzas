f4d1fe144526ceb816f47e0b90b9bf4550828cbd
astechtechnologie <88607697+astechtechnologie@users.noreply.github.com>
2026-08-11 16:37:15 -0500

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,16 @@
+// Configuración e inicialización de Firebase
+(function() {
+  const firebaseConfig = {
+    apiKey: "AIzaSyAD94mJ0gFuFGvRwhhrn8qTFe3orsO_sDA",
+    authDomain: "finanzas-personales-a1ee9.firebaseapp.com",
+    projectId: "finanzas-personales-a1ee9",
+    storageBucket: "finanzas-personales-a1ee9.firebasestorage.app",
+    messagingSenderId: "253217416131",
+    appId: "1:253217416131:web:ce754c98c1e062f9c45376",
+    measurementId: "G-K645PYV7JP"
+  };
+  firebase.initializeApp(firebaseConfig);
+  window.App = window.App || {};
+  window.App.auth = firebase.auth();
+  window.App.db = firebase.firestore();
+})();
