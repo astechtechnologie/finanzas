6d88b1b87f339852709fc4d87dc819fd7db95c0d
astechtechnologie <88607697+astechtechnologie@users.noreply.github.com>
2026-09-06 11:37:24 -0500

    Increment CACHE_NAME to finanzas-v11.8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'finanzas-v11.7'; // ← Incrementa este número en cada actualización
+const CACHE_NAME = 'finanzas-v11.8'; // ← Incrementa este número en cada actualización
 
 const ARCHIVOS_CACHE = [
   './',
