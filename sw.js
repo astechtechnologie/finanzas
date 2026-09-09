6caa0c12c7242094fe68adbfeb493fe1b0669a58
astechtechnologie <88607697+astechtechnologie@users.noreply.github.com>
2026-09-08 19:25:55 -0500

    Update CACHE_NAME to finanzas-v11.10
    Increment cache version for updates.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'finanzas-v11.9'; // ← Incrementa este número en cada actualización
+const CACHE_NAME = 'finanzas-v11.10'; // ← Incrementa este número en cada actualización
 
 const ARCHIVOS_CACHE = [
   './',
